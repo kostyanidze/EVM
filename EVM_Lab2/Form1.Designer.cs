8cd0638c4c89226884f04f0c523c753b61aa493c
kostyanidze <kostyanidze173@gmail.com>
2019-12-11 06:56:07 +0400

    3

--- EVM_Lab2/Form1.Designer.cs
+++ EVM_Lab2/Form1.Designer.cs
@@ -40,6 +40,11 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBoxSign = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -77,7 +82,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(280, 20);
+            this.label1.Location = new System.Drawing.Point(580, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 4;
@@ -85,7 +90,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(280, 67);
+            this.label2.Location = new System.Drawing.Point(580, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 5;
@@ -93,7 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(280, 110);
+            this.label3.Location = new System.Drawing.Point(580, 107);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 6;
@@ -101,7 +106,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(280, 163);
+            this.label4.Location = new System.Drawing.Point(580, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 13);
             this.label4.TabIndex = 7;
@@ -110,7 +115,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(349, 20);
+            this.label5.Location = new System.Drawing.Point(649, 17);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 16);
             this.label5.TabIndex = 8;
@@ -120,7 +125,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(349, 67);
+            this.label6.Location = new System.Drawing.Point(649, 64);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(105, 16);
             this.label6.TabIndex = 9;
@@ -130,7 +135,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(349, 110);
+            this.label7.Location = new System.Drawing.Point(649, 107);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(86, 16);
             this.label7.TabIndex = 10;
@@ -140,17 +145,62 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(349, 163);
+            this.label8.Location = new System.Drawing.Point(649, 160);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(146, 16);
             this.label8.TabIndex = 11;
             this.label8.Text = "Шестнадцатиричная";
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(285, 13);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(137, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(285, 60);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(137, 20);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(285, 103);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(137, 20);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(285, 156);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(137, 20);
+            this.textBox8.TabIndex = 15;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBoxSign
+            // 
+            this.textBoxSign.Location = new System.Drawing.Point(207, 84);
+            this.textBoxSign.Name = "textBoxSign";
+            this.textBoxSign.Size = new System.Drawing.Size(16, 20);
+            this.textBoxSign.TabIndex = 16;
+            this.textBoxSign.TextChanged += new System.EventHandler(this.textBoxSign_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(517, 211);
+            this.ClientSize = new System.Drawing.Size(807, 211);
+            this.Controls.Add(this.textBoxSign);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -184,6 +234,11 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBoxSign;
     }
 }
 
--- EVM_Lab2/Form1.cs
+++ EVM_Lab2/Form1.cs
@@ -22,13 +22,10 @@ namespace EVM_Lab2
             try
             {
                 int convertedFromBinary = Convert.ToInt32(textBox1.Text);
-                textBox2.Text = convertToOctal(convertedFromBinary,2).ToString();
+                textBox2.Text = convertToOctal(convertedFromBinary, 2).ToString();
                 textBox3.Text = convertToDecimal(convertedFromBinary, 2).ToString();
                 textBox4.Text = convertToHexadecimal(convertedFromBinary, 2).ToString();
-                label1.Text = convertedFromBinary.ToString();
-                label2.Text = convertToDecimal(convertedFromBinary, 2).ToString();
-                label3.Text = convertToOctal(convertedFromBinary, 2).ToString();
-                label4.Text = convertToHexadecimal(convertedFromBinary, 2);
+
             }
             catch (FormatException)
             {
@@ -47,10 +44,7 @@ namespace EVM_Lab2
                 textBox1.Text = convertToBinary(convertedFromOctal, 8).ToString();
                 textBox3.Text = convertToDecimal(convertedFromOctal, 8).ToString();
                 textBox4.Text = convertToHexadecimal(convertedFromOctal, 8).ToString();
-                label1.Text = convertToBinary(convertedFromOctal, 8).ToString();
-                label2.Text = convertedFromOctal.ToString();
-                label3.Text = convertToDecimal(convertedFromOctal, 8).ToString();
-                label4.Text = convertToHexadecimal(convertedFromOctal, 8);
+
             }
             catch (FormatException)
             {
@@ -69,10 +63,7 @@ namespace EVM_Lab2
                 textBox1.Text = convertToBinary(convertedFromDecimal, 10).ToString();
                 textBox2.Text = convertToOctal(convertedFromDecimal, 10).ToString();
                 textBox4.Text = convertToHexadecimal(convertedFromDecimal, 10).ToString();
-                label1.Text = convertToBinary(convertedFromDecimal, 10).ToString();
-                label2.Text = convertToOctal(convertedFromDecimal, 10).ToString();
-                label3.Text = convertToDecimal(convertedFromDecimal, 10).ToString();
-                label4.Text = convertToHexadecimal(convertedFromDecimal, 10);
+
             }
             catch (FormatException)
             {
@@ -91,10 +82,7 @@ namespace EVM_Lab2
                 textBox1.Text = convertToBinary(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
                 textBox2.Text = convertToOctal(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
                 textBox3.Text = convertFromHexaToDecimal(hexadecimalNumber).ToString();
-                label1.Text = convertToBinary(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
-                label2.Text = convertToOctal(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
-                label3.Text = convertFromHexaToDecimal(hexadecimalNumber).ToString();
-                label4.Text = textBox4.Text;
+
             }
             catch (FormatException)
             {
@@ -104,6 +92,98 @@ namespace EVM_Lab2
                 label4.Text = "Wrong enter";
             }
         }
+
+        private void textBoxSign_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string sign = textBoxSign.Text;
+                label1.Text = Operations(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox5.Text), sign).ToString();
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Enter the numbers";
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int convertedFromBinary = Convert.ToInt32(textBox5.Text);
+                textBox6.Text = convertToOctal(convertedFromBinary, 2).ToString();
+                textBox7.Text = convertToDecimal(convertedFromBinary, 2).ToString();
+                textBox8.Text = convertToHexadecimal(convertedFromBinary, 2).ToString();
+
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Wrong enter";
+                label2.Text = "Wrong enter";
+                label3.Text = "Wrong enter";
+                label4.Text = "Wrong enter";
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int convertedFromOctal = Convert.ToInt32(textBox6.Text);
+                textBox5.Text = convertToBinary(convertedFromOctal, 8).ToString();
+                textBox7.Text = convertToDecimal(convertedFromOctal, 8).ToString();
+                textBox8.Text = convertToHexadecimal(convertedFromOctal, 8).ToString();
+
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Wrong enter";
+                label2.Text = "Wrong enter";
+                label3.Text = "Wrong enter";
+                label4.Text = "Wrong enter";
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int convertedFromDecimal = Convert.ToInt32(textBox7.Text);
+                textBox5.Text = convertToBinary(convertedFromDecimal, 10).ToString();
+                textBox6.Text = convertToOctal(convertedFromDecimal, 10).ToString();
+                textBox8.Text = convertToHexadecimal(convertedFromDecimal, 10).ToString();
+
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Wrong enter";
+                label2.Text = "Wrong enter";
+                label3.Text = "Wrong enter";
+                label4.Text = "Wrong enter";
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string hexadecimalNumber = textBox8.Text;
+                textBox5.Text = convertToBinary(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
+                textBox6.Text = convertToOctal(convertFromHexaToDecimal(hexadecimalNumber), 10).ToString();
+                textBox7.Text = convertFromHexaToDecimal(hexadecimalNumber).ToString();
+
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Wrong enter";
+                label2.Text = "Wrong enter";
+                label3.Text = "Wrong enter";
+                label4.Text = "Wrong enter";
+            }
+        }
+
         public int convertToBinary(int number, int numOfSys)
         {
             if (numOfSys == 10)
@@ -279,6 +359,250 @@ namespace EVM_Lab2
                 }
             }
             return convertedNumber;
+        }
+
+        public int Operations(int firstNumber, int secondNumber, string sign)
+        {
+            int number = 123;
+            int a, b, bonus = 0;
+            string sum = "";
+            string subt = "";
+            string temp1 = "";
+            if (sign == "+")
+            {
+                bool fnumgreater;
+                if (firstNumber > secondNumber)
+                {
+                    fnumgreater = true;
+                }
+                else
+                {
+                    fnumgreater = false;
+                }
+                if (fnumgreater == false)
+                {
+                    while (firstNumber > 0)
+                    {
+                        a = firstNumber % 10;
+                        b = secondNumber % 10;
+                        firstNumber /= 10;
+                        secondNumber /= 10;
+                        if (a + b == 2)
+                        {
+                            if (bonus == 0)
+                            {
+                                sum = 0 + sum;
+                                bonus = 1;
+                            }
+                            else if (bonus == 1)
+                            {
+                                sum = bonus + sum;
+                                bonus = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (bonus == 0)
+                            {
+                                sum = (a + b) + sum;
+                            }
+                            else if (bonus == 1)
+                            {
+                                if (a == 1 || b == 1)
+                                {
+                                    bonus = 1;
+                                    sum = 0 + sum;
+                                }
+                                else if (a == 0 && b == 0)
+                                {
+                                    sum = 1 + sum;
+                                    bonus = 0;
+                                }
+                            }
+                        }
+                    }
+                    while (secondNumber > 0)
+                    {
+                        if (bonus == 1)
+                        {
+                            b = secondNumber % 10;
+                            secondNumber /= 10;
+                            if (b == 0)
+                            {
+                                sum = 1 + sum;
+                                bonus = 0;
+                            }
+                            else if (b == 1)
+                            {
+                                sum = 0 + sum;
+                                bonus = 1;
+                            }
+
+                        }
+                        else
+                        {
+                            sum = secondNumber + sum;
+                            bonus = 0;
+                            break;
+                        }
+                    }
+                    if (bonus == 1)
+                    {
+                        sum = 1 + sum;
+                    }
+
+                }
+                else if (fnumgreater == true)
+                {
+                    while (secondNumber > 0)
+                    {
+                        a = firstNumber % 10;
+                        b = secondNumber % 10;
+                        firstNumber /= 10;
+                        secondNumber /= 10;
+                        if (a + b == 2)
+                        {
+                            if (bonus == 0)
+                            {
+                                sum = 0 + sum;
+                                bonus = 1;
+                            }
+                            else if (bonus == 1)
+                            {
+                                sum = bonus + sum;
+                                bonus = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (bonus == 0)
+                            {
+                                sum = (a + b) + sum;
+                            }
+                            else if (bonus == 1)
+                            {
+                                if (a == 1 || b == 1)
+                                {
+                                    bonus = 1;
+                                    sum = 0 + sum;
+                                }
+                                else if (a == 0 && b == 0)
+                                {
+                                    sum = 1 + sum;
+                                    bonus = 0;
+                                }
+                            }
+                        }
+                    }
+                    while (firstNumber > 0)
+                    {
+                        if (bonus == 1)
+                        {
+                            b = firstNumber % 10;
+                            firstNumber /= 10;
+                            if (b == 0)
+                            {
+                                sum = 1 + sum;
+                                bonus = 0;
+                            }
+                            else if (b == 1)
+                            {
+                                sum = 0 + sum;
+                                bonus = 1;
+                            }
+
+                        }
+                        else
+                        {
+                            sum = firstNumber + sum;
+                            bonus = 0;
+                            break;
+                        }
+                    }
+                    if (bonus == 1)
+                    {
+                        sum = 1 + sum;
+                    }
+                }
+                number = Convert.ToInt32(sum);
+                bonus = 0;
+            }
+
+            if (sign == "-")
+            {
+
+                while (firstNumber > 0)
+                {
+                    a = firstNumber % 10;
+                    b = secondNumber % 10;
+                    firstNumber /= 10;
+                    secondNumber /= 10;
+                    if (a - b == 0)
+                    {
+                        subt = 0 + subt;
+                    }
+                    else if (a - b == 1)
+                    {
+                        subt = 1 + subt;
+                    }
+                    else if(a-b == -1)
+                    {
+                        int i = 0;
+                        while(firstNumber % 10 != 1)
+                        {
+                            temp1 = (firstNumber / 10).ToString();
+                            firstNumber /= 10;
+                            int j = 1;
+                            
+                            while (j < i)
+                            {
+                                temp1 = firstNumber.ToString() + 1;
+                                j++;
+                            }
+                            i = 0;
+                            i++;
+                        }
+                        temp1 = (firstNumber/10).ToString() + 0 +temp1;
+                        firstNumber = Convert.ToInt32(temp1);
+                        subt = 1 + subt;
+                    }
+                }
+                number = Convert.ToInt32(subt);
+            }
+
+            if (sign == "*")
+            {
+                int i = 0;
+                int j = 0;
+                int k = 1;
+                int m = 0;
+                int n = 0;
+                string summ = "";
+                int result = 0;
+                int temp = firstNumber;
+                Console.WriteLine(firstNumber.ToString().Length);
+                for (i = 0; i < secondNumber.ToString().Length; i++)
+                {
+                   
+                    firstNumber = temp;
+                    m = secondNumber % 10;
+                    secondNumber /= 10;
+                    for (j = 0; j < firstNumber.ToString().Length; j++)
+                    {
+                        n = firstNumber % 10;
+                        firstNumber /= 10;
+                        int test = n * m;
+                        Console.WriteLine("test=" + test + "n="+n+"m="+m);
+                        summ = summ+(n * m).ToString();
+                        Console.WriteLine(summ);
+                        result = result + (Convert.ToInt32(summ) * k);
+                    }
+                    k*=10;
+                }
+                number = result;
+            }
+
+            return number;
         }
     }
 }
